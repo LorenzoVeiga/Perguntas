d565b77e7b812a1da7ea703dc23b11463f849337
LorenzoVeiga <satti.lorenzo@escola.pr.gov.br>
2024-10-29 14:40:45 +0000

    iuiio

---
+++ perguntas.js
@@ -0,0 +1,118 @@
+criaCartao(
+    'Química',
+    'O que é a Lei de Conservação da Massa e como ela se aplica nas reações químicas ?',
+    'A Lei de Conservação da Massa afirma que a massa total dos reagentes é igual à massa total dos produtos em uma reação química, ou seja, a massa não é criada nem destruída durante a reação.')
+
+criaCartao(
+    'Química',
+    'Qual é a principal diferença entre uma solução saturada e uma solução supersaturada ?',
+    'Uma solução saturada contém a máxima quantidade de soluto que pode ser dissolvida em um solvente a uma dada temperatura, enquanto uma solução supersaturada contém mais soluto do que o normalmente permitido a essa temperatura.')
+
+    criaCartao(
+        'Química',
+        'Oque é entalpia ?',
+        'É uma função termodinâmica pela qual se calcula o calor envolvido em processos isobáricos')
+
+criaCartao(
+    'Química',
+    'Qual fórmula da concentração comum ?',
+    'C = m1 / v.')
+
+criaCartao(
+    'Química',
+    'Qual fórmula do Título?',
+    'T = m1 / m1 + m2.')
+
+criaCartao(
+    'Química',
+    'Concentração molar ?',
+    'M = m1 / MM.v.')
+
+criaCartao(
+    'Química',
+    'Qual fórmula da densidade ?',
+    'd = m / v.')
+        
+
+
+
+
+criaCartao(
+    'Biologia',
+    'Como são transmitidas a Leishmaniose e a dengue?',
+    'As duas podem ser transmitidas pelo Aedes aegypti')
+
+
+
+
+criaCartao(
+    'Geografia',
+    'Oque é ilha de calor ?',
+    'As ilhas de calor são um fenômeno que ocorre naquelas áreas urbanas que apresentam temperaturas mais altas pela atividade humana que as zonas circundantes.')
+
+criaCartao(
+    'Geográfia',
+    'Quais são os principais tipos de clima encontrados no Brasil e em que regiões do país eles predominam ?',
+    'Os principais tipos de clima no Brasil são o tropical(predomina na Amazônia e no Centro - Oeste), o subtropical(presente no Sul), o semiárido(Nordeste) e o equatorial(na região Norte).')
+
+
+
+
+criaCartao(
+    'Sociologia',
+    'O que é o conceito de "capital social" e como ele influencia as relações sociais ?',
+    'Capital social refere - se aos recursos e benefícios que uma pessoa pode acessar por meio de suas redes sociais e relações.Ele influencia a capacidade de obter apoio social, informações e oportunidades.')
+
+
+
+
+criaCartao(
+    'História',
+    'Qual o principal representante da reforma calvinista ?',
+    'João Calvinista.')
+
+criaCartao(
+    'História',
+    'Qual foi o estopim da primeira guerra mundial ?',
+    'A morte do arquiduque Francisco Ferdinando')
+
+
+
+
+criaCartao(
+    'Física',
+    'Qual fórmula da velocidade média ?',
+    'V = S / T.')
+
+criaCartao(
+    'Física',
+    'Qual a fórmula da termodinâmic ?',
+    'Q = CMΔT')
+
+
+
+
+criaCartao(
+    'Matemática',
+    'Como calcular os ângulos entre ponteiros de um relógio',
+    'Ângulos = | 60h - 11min | / 2')
+
+criaCartao(
+    'Matemática',
+    'Qual fórmula da geometria analítica ?',
+    'D = √(Xb - Xa)² + (Yb - Ya)².')
+
+criaCartao(
+    'Matemática',
+    'Qual ângulo notável do cos?',
+    '30°= √3 / 2  |  45° = √2 / 2  |  60° = 1 / 2.')
+
+criaCartao(
+    'Matemática',
+    'Qual ângulo notável do sem?',
+    '30°= 1 / 2   |  45° = √2 / 2  |  60° = √3 / 2.')
+
+criaCartao(
+    'Matemática',
+    'Qual os ângulos notáveis da tangente?',
+    '30° = √3 / 3    |    45° = 1    |   60° = √3.')
