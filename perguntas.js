a11376cfea5f4ae3c7aa1a1e406fa8f003ca36f7
LorenzoVeiga <satti.lorenzo@escola.pr.gov.br>
2024-10-29 14:45:57 +0000

    fdusfhshfs

--- perguntas.js
+++ perguntas.js
@@ -1,41 +1,3 @@
-criaCartao(
-    'Química',
-    'O que é a Lei de Conservação da Massa e como ela se aplica nas reações químicas ?',
-    'A Lei de Conservação da Massa afirma que a massa total dos reagentes é igual à massa total dos produtos em uma reação química, ou seja, a massa não é criada nem destruída durante a reação.')
-
-criaCartao(
-    'Química',
-    'Qual é a principal diferença entre uma solução saturada e uma solução supersaturada ?',
-    'Uma solução saturada contém a máxima quantidade de soluto que pode ser dissolvida em um solvente a uma dada temperatura, enquanto uma solução supersaturada contém mais soluto do que o normalmente permitido a essa temperatura.')
-
-    criaCartao(
-        'Química',
-        'Oque é entalpia ?',
-        'É uma função termodinâmica pela qual se calcula o calor envolvido em processos isobáricos')
-
-criaCartao(
-    'Química',
-    'Qual fórmula da concentração comum ?',
-    'C = m1 / v.')
-
-criaCartao(
-    'Química',
-    'Qual fórmula do Título?',
-    'T = m1 / m1 + m2.')
-
-criaCartao(
-    'Química',
-    'Concentração molar ?',
-    'M = m1 / MM.v.')
-
-criaCartao(
-    'Química',
-    'Qual fórmula da densidade ?',
-    'd = m / v.')
-        
-
-
-
 
 criaCartao(
     'Biologia',
@@ -116,3 +78,40 @@ criaCartao(
     'Matemática',
     'Qual os ângulos notáveis da tangente?',
     '30° = √3 / 3    |    45° = 1    |   60° = √3.')
+
+    
+
+criaCartao(
+    'Química',
+    'O que é a Lei de Conservação da Massa e como ela se aplica nas reações químicas ?',
+    'A Lei de Conservação da Massa afirma que a massa total dos reagentes é igual à massa total dos produtos em uma reação química, ou seja, a massa não é criada nem destruída durante a reação.')
+
+criaCartao(
+    'Química',
+    'Qual é a principal diferença entre uma solução saturada e uma solução supersaturada ?',
+    'Uma solução saturada contém a máxima quantidade de soluto que pode ser dissolvida em um solvente a uma dada temperatura, enquanto uma solução supersaturada contém mais soluto do que o normalmente permitido a essa temperatura.')
+
+criaCartao(
+    'Química',
+    'Oque é entalpia ?',
+    'É uma função termodinâmica pela qual se calcula o calor envolvido em processos isobáricos')
+
+criaCartao(
+    'Química',
+    'Qual fórmula da concentração comum ?',
+    'C = m1 / v.')
+
+criaCartao(
+    'Química',
+    'Qual fórmula do Título?',
+    'T = m1 / m1 + m2.')
+
+criaCartao(
+    'Química',
+    'Concentração molar ?',
+    'M = m1 / MM.v.')
+
+criaCartao(
+    'Química',
+    'Qual fórmula da densidade ?',
+    'd = m / v.')
